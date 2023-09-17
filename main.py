8521b41c759443668a6af68c9a4dfc70617b55e5
Iago <iagotpn@gmail.com>
2023-09-17 00:56:18 -0300

    mostrando dados da requisição

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ api_base_url = f'https://api.github.com/users/{username}'
 
 r = requests.get(api_base_url)
 print(r.status_code)
-print(r.json())
-print(r.url)
+
+dados = r.json()
+
+print(f"Nome do usuário: {dados['name']}")
+print(f"Nome do usuário: {dados['login']}")
+print(f"Nome do usuário: {dados['created_at']}")
+print(f"Nome do usuário: {dados['public_repos']}")
